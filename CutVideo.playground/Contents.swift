14e5776d3b7295f586164819aed6576bf49c07d6
Gleb Karpushkin <Luccifer@users.noreply.github.com>
2018-03-09 02:31:36 +0300

    Update Contents.swift

--- CutVideo.playground/Contents.swift
+++ CutVideo.playground/Contents.swift
@@ -4,25 +4,34 @@ import UIKit
 import AVKit
 import PlaygroundSupport
 
-let pgDir = playgroundSharedDataDirectory
-let fileURL = pgDir.appendingPathComponent("input.mp4")
+// Create folder manually: /Users/{USER}/Documents/Shared\ Playground\
+// Also create images folder inside
+
+let playGorundDirectory = playgroundSharedDataDirectory
+
+let fileURL = playGorundDirectory.appendingPathComponent("input.mp4")
 let asset = AVAsset(url: fileURL)
 var vidLength: CMTime = asset.duration
 var seconds: Double = CMTimeGetSeconds(vidLength)
-var requiredFramesCount = Int((seconds * 10))
+
+var requiredFramesCount = Int((seconds * 24))
+
 var step = Int((vidLength.value / Int64(requiredFramesCount)))
 var value: Int = 0
-for i in 0..<requiredFramesCount {
-    let imageGenerator = AVAssetImageGenerator(asset: asset)
-    imageGenerator.requestedTimeToleranceAfter = kCMTimeZero
-    imageGenerator.requestedTimeToleranceBefore = kCMTimeZero
-    let time: CMTime = CMTimeMake(Int64(value), vidLength.timescale)
-    let imageRef = (try? imageGenerator.copyCGImage(at: time, actualTime: nil))
-    let thumbnail = UIImage(cgImage: imageRef!)
-    let filename = "/Users/glebkarpushkin/Downloads/videoImages/frame_\(i).png"
-    let pngPath: URL = URL(fileURLWithPath: NSHomeDirectory()).appendingPathComponent(filename)
-    let image = UIImagePNGRepresentation(thumbnail)
-    try! image?.write(to: URL(string: "\(pgDir)/images/frame_\(i).png")!)
-    value += Int(step)
+
+func iterate() {
+    for i in 0..<requiredFramesCount {
+        let imageGenerator = AVAssetImageGenerator(asset: asset)
+        imageGenerator.requestedTimeToleranceAfter = kCMTimeZero
+        imageGenerator.requestedTimeToleranceBefore = kCMTimeZero
+        let time: CMTime = CMTimeMake(Int64(value), vidLength.timescale)
+        let imageRef = (try? imageGenerator.copyCGImage(at: time, actualTime: nil))
+        let thumbnail = UIImage(cgImage: imageRef!)
+        let image = UIImagePNGRepresentation(thumbnail)
+        guard let urlToSave = URL(string: "\(playGorundDirectory)/images/frame_\(i).png") else {return}
+        try! image?.write(to: urlToSave)
+        value += Int(step)
+    }
 }
 
+iterate()
